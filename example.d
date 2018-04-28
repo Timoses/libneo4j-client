47357a460605a070a68a686722af6b4090c826f4
Timoses <timosesu@gmail.com>
2018-04-28 15:51:36 +0200

    Add example.d

---
+++ example.d
@@ -0,0 +1,29 @@
+// Compile with:
+//  rdmd -L-lneo4j-client
+
+import deimos.neo4j.client;
+
+enum dbUri = "neo4j://neo4j:test@localhost:7687";
+
+int main()
+{
+    neo4j_client_init();
+
+    import std.string : toStringz;
+    neo4j_connection_t* connection =
+        neo4j_connect(toStringz(dbUri), null, NEO4J_INSECURE);
+
+    if (connection is null)
+    {
+        import core.stdc.stdio : stderr;
+        import core.stdc.errno : errno;
+
+        neo4j_perror(stderr, errno, "Connection failed");
+        return 1;
+    }
+
+    neo4j_close(connection);
+    neo4j_client_cleanup();
+
+    return 0;
+}
